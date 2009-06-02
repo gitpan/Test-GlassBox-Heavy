f5a10dfb964607bdf0124d57e30da1e9002034bc
Oliver Gorwits <oliver@cpan.org>
2009-06-02 09:44:53 +0000

    Import of OLIVER/Test-GlassBox-Heavy-1.03 from CPAN.
    gitpan-cpan-distribution: Test-GlassBox-Heavy
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         OLIVER/Test-GlassBox-Heavy-1.03.tar.gz
    gitpan-cpan-author:       OLIVER
    gitpan-cpan-maturity:     released

--- lib/Test/GlassBox/Heavy.pm
+++ lib/Test/GlassBox/Heavy.pm
@@ -14,10 +14,10 @@ use Devel::Symdump;
 use File::Slurp;
 use Carp;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
-# $Id: Heavy.pm 475 2009-06-02 09:35:09Z oliver $
+# $Id: Heavy.pm 476 2009-06-02 09:43:20Z oliver $
 
 our @used_modules;
 BEGIN {
@@ -121,7 +121,7 @@ Test::GlassBox::Heavy - Non-invasive testing of subroutines within Perl programs
 
 =head1 VERSION
 
-This document refers to version 1.02 of Test::GlassBox::Heavy
+This document refers to version 1.03 of Test::GlassBox::Heavy
 
 =head1 SYNOPSIS
 
